a6ae44e42d5685f2c2f1fee61a149cabfac1276d
Leonardo Shinagawa <leonardo.shinagawa@hotelbird.com>
2024-04-10 19:15:26 +0200

    feat: add command to install user roles

---
+++ app/Console/Commands/InstallUserRolesCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\Role as RoleEnum;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Spatie\Permission\Models\Role;
+
+class InstallUserRolesCommand extends Command
+{
+    protected $signature = 'install:user-roles';
+
+    protected $description = 'Install Roles and set the admin role to the first user';
+
+    public function handle(): void
+    {
+        RoleEnum::collect()
+            ->each(fn (RoleEnum $role) => Role::createOrFirst(['name' => $role->value]));
+
+        User::first()->assignRole(RoleEnum::ADMIN);
+    }
+}
