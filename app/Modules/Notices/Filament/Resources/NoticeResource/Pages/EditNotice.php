a5bf2db36a2327be94768a16469e16eb5ad7da58
Leonardo Shinagawa <leonardo.shinagawa@hotelbird.com>
2024-04-14 16:54:58 +0200

    fix: translate when notice is changed

--- app/Modules/Notices/Filament/Resources/NoticeResource/Pages/EditNotice.php
+++ app/Modules/Notices/Filament/Resources/NoticeResource/Pages/EditNotice.php
@@ -3,6 +3,8 @@
 namespace App\Modules\Notices\Filament\Resources\NoticeResource\Pages;
 
 use App\Modules\Notices\Filament\Resources\NoticeResource;
+use App\Modules\Notices\Notice;
+use App\Modules\Notices\Services\TranslateNotice;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
@@ -15,5 +17,15 @@ class EditNotice extends EditRecord
         return [
             DeleteAction::make(),
         ];
+    }
+
+    public function beforeSave()
+    {
+        Notice::saving(function (Notice $notice) {
+            if ($notice->isDirty('content')) {
+                $translateNotice = resolve(TranslateNotice::class);
+                $translateNotice($notice);
+            }
+        });
     }
 }
