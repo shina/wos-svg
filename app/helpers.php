3d71570f5bef4f089472defdb916fae2b6239289
Leonardo Shinagawa <leonardo.shinagawa@hotelbird.com>
2024-04-21 13:03:08 -0300

    fix: Wrap 'objUse' function in 'function_exists' check

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,8 @@
 <?php
 
-function objUse(object $obj, string $traitName): bool
-{
-    return in_array($traitName, class_uses($obj));
+if (! function_exists('objUse')) {
+    function objUse(object $obj, string $traitName): bool
+    {
+        return in_array($traitName, class_uses($obj));
+    }
 }
