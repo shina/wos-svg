692bd05b95839f8f79b010dba84f7488d63a79d1
Leonardo Shinagawa <leonardo.shinagawa@hotelbird.com>
2024-04-26 12:32:30 -0300

    chore: install tailwind and dummy homepage

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use App\Modules\Wiki\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', fn () => view('welcome'));
 Route::get('/{slug}', PageController::class);
