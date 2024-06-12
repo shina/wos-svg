36c5007eeb924fb2afd11c788d8b1e8609c191ee
Leonardo Shinagawa <leonardo.shinagawa@hotelbird.com>
2024-06-12 18:51:55 +0200

    feat: modify the TrustLevelColumn to be a group of 3 columns

--- app/Modules/Participation/Filament/Resources/EventResource/Table/Columns/TrustLevelColumn.php
+++ app/Modules/Participation/Filament/Resources/EventResource/Table/Columns/TrustLevelColumn.php
@@ -4,6 +4,9 @@ namespace App\Modules\Participation\Filament\Resources\EventResource\Table\Colum
 
 use App\Models\Player;
 use App\Modules\Participation\Services\CalculateTrustLevel;
+use App\Modules\Participation\Services\CalculateTrustLevel\QueryModifiers\Last3Events;
+use App\Modules\Participation\Services\CalculateTrustLevel\QueryModifiers\OneMonth;
+use Filament\Tables\Columns\ColumnGroup;
 use Filament\Tables\Columns\TextColumn;
 
 class TrustLevelColumn
@@ -16,14 +19,35 @@ class TrustLevelColumn
      */
     public static function make(callable $getPlayerId)
     {
-        return TextColumn::make('trust-level')
-            ->state(function (CalculateTrustLevel $calculateTrustLevel, $record) use ($getPlayerId) {
-                return rescue(
-                    fn () => $calculateTrustLevel->player($getPlayerId($record)),
-                    'never attended',
-                    false
-                );
-            })
-            ->suffix(fn (string $state) => $state === 'never attended' ? '' : '%');
+        return ColumnGroup::make('Participation rate')
+            ->columns([
+                TextColumn::make('last-3-events')
+                    ->state(function (CalculateTrustLevel $calculateTrustLevel, $record) use ($getPlayerId) {
+                        return rescue(
+                            fn () => $calculateTrustLevel->player($getPlayerId($record), new Last3Events()),
+                            'never attended',
+                            false
+                        );
+                    })
+                    ->suffix(fn (string $state) => $state === 'never attended' ? '' : '%'),
+                TextColumn::make('1-month')
+                    ->state(function (CalculateTrustLevel $calculateTrustLevel, $record) use ($getPlayerId) {
+                        return rescue(
+                            fn () => $calculateTrustLevel->player($getPlayerId($record), new OneMonth()),
+                            'never attended',
+                            false
+                        );
+                    })
+                    ->suffix(fn (string $state) => $state === 'never attended' ? '' : '%'),
+                TextColumn::make('all-time')
+                    ->state(function (CalculateTrustLevel $calculateTrustLevel, $record) use ($getPlayerId) {
+                        return rescue(
+                            fn () => $calculateTrustLevel->player($getPlayerId($record)),
+                            'never attended',
+                            false
+                        );
+                    })
+                    ->suffix(fn (string $state) => $state === 'never attended' ? '' : '%'),
+            ]);
     }
 }
