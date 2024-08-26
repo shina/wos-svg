ca0b7f2238b5224a2c93383ad876510ea1157ac9
Leonardo Shinagawa <leonardo.shinagawa@hotelbird.com>
2024-08-26 20:47:31 +0200

    feat(notices): Add alliance association

--- app/Modules/Notices/Notice.php
+++ app/Modules/Notices/Notice.php
@@ -3,12 +3,14 @@
 namespace App\Modules\Notices;
 
 use App\Modules\Notices\database\factories\NoticeFactory;
+use App\Traits\BelongsToAlliance;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Notice extends Model
 {
+    use BelongsToAlliance;
     use HasFactory;
 
     protected $guarded = [];
