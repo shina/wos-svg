49c8b60ab2f4fe2b8f053b85e928c70dc0da6f40
Leonardo Shinagawa <leonardo.shinagawa@hotelbird.com>
2024-09-10 22:18:27 +0200

    fix: test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,29 @@
 
 namespace Tests;
 
+use App\AllianceSelector;
+use App\Models\Alliance;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        app()->bind(AllianceSelector::class, function () {
+            return new class
+            {
+                public function select(Alliance $alliance)
+                {
+                    //
+                }
+
+                public function getSelected(): int
+                {
+                    return 1;
+                }
+            };
+        });
+    }
 }
