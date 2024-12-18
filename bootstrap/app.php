570ea2d36370ac96075a151a7f85c9d68c92edc6
Leonardo Shinagawa <leonardo.shinagawa@hotelbird.com>
2024-12-18 22:03:57 +0100

    trust all proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware
+            ->trustProxies('*')
             ->web(
                 append: [
                     AllianceSetupMiddleware::class,
